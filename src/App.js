feb8993997d518d2095ae129c5f6df4c73125478
Casey Calkins <c.angelcalkins@outlook.com>
2020-12-12 19:35:42 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainButtons from './components/Buttons/AddandSaved';
+import './App.css';
+
+function App() {
+  return (
+    <div  className="container">
+      <h1>Shopping List</h1>
+    <MainButtons />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Buttons/AddandSaved.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function MainButtons() {
+    return (
+        <div>
+            <div className='add-btn'>
+                <button>I'm a button</button>
+            </div>
+
+            <div className="save-btn">
+                <button>I'm also a button</button>
+            </div>
+        </div>
+    )
+}
